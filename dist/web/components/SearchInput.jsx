bd8ff01683aa410efe6f7508b753d6ac148efb6b
Sandeep Somavarapu <sandy081@gmail.com>
2016-03-06 18:17:20 +0100

    Working State

--- dist/web/components/SearchInput.jsx
+++ dist/web/components/SearchInput.jsx
@@ -5,8 +5,8 @@ var SearchInput= React.createClass({
     render: function() {
         return (
                 <div className="searchInputContainer">
-                      <input ref="disabledInput" type="search" autoComplete="off" disabled="disabled"/>
-                      <input ref="enabledInput" className="enabledInput" type="search" autoComplete="off"
+                      <input type="search" autoComplete="off" disabled="disabled"/>
+                      <input className="enabledInput" type="search" autoComplete="off"
                              placeholder="Type to search..."/>
                 </div>
               );
