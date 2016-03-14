cc7a46d11fc522a307e1c1fc70107436b24db165
Sandeep Somavarapu <sandy081@gmail.com>
2016-03-14 15:00:23 +0100

    Merge Data

--- dist/server/flights/models/Data.js
+++ dist/server/flights/models/Data.js
@@ -17,6 +17,24 @@ exports.Attributes= function() {
      };
 }
 
+exports.columns= function() { 
+return [
+        "Airlines",
+        "From",
+        "To",
+        "Price"
+    ];
+}
+
+exports.flights= function() {
+    return [
+        {From: "Zurich", To: "Madrid", Airlines: {iconUrl: "/images/swiss.png", value:"Swiss Air"}, Adult:100, Child:90, Infant: 10, '#': 5},   
+        {From: "Zurich", To: "Madrid", Airlines: {iconUrl: "/images/lufthansa.png", value:"Lufthansa"}, Adult:80, Child:70, Infant: 10, '#': 2},   
+        {From: "Zurich", To: "Madrid", Airlines: {iconUrl: "/images/airfrance.png", value:"Air France"}, Adult:95, Child:80, Infant: 10, '#': 3},   
+        {From: "Zurich", To: "Madrid", Airlines: {iconUrl: "/images/austrian.png", value:"Austrian Airlines"}, Adult:85, Child:75, Infant: 10, '#': 1}   
+    ];
+}
+
 exports.Airports= function() {
     return [
         
